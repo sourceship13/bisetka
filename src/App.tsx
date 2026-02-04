7647768bea617b5e13d09911eaaaf6f700542084
ajp-sourceship <ajp2914@gmail.com>
2026-02-04 00:57:31 -0600

    feat: implement authentication flow with login and home screens

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator';
+import 'react-native-gesture-handler';
+
+function App(): React.JSX.Element {
+  return <AppNavigator />;
+}
+
+export default App;
