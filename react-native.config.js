03e11a9fd9f778e42afcf81b3fcef26bb2f39b79
ajp-sourceship <ajp2914@gmail.com>
2026-03-13 00:45:30 -0500

    -Add FontTestScreen to showcase available fonts with live preview functionality -Add 10 Fonts for Card Generation

--- react-native.config.js
+++ react-native.config.js
@@ -3,6 +3,7 @@
  * @type {import('@react-native-community/cli-types').UserDependencyConfig}
  */
 module.exports = {
+  assets: ['./assets/fonts/'],
   dependencies: {
     // Dependencies are auto-detected. Add overrides here if needed.
   },
