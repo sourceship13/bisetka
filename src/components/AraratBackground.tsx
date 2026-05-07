a9df0fcf8cc694d43f892809c498847457197245
ajp-sourceship <ajp2914@gmail.com>
2026-05-07 02:06:28 -0500

    feat: add PoolTable3D component to render 3D pool table model in WebView

--- src/components/AraratBackground.tsx
+++ src/components/AraratBackground.tsx
@@ -11,7 +11,7 @@
 import React from 'react';
 import {ImageBackground, StyleSheet, View} from 'react-native';
 
-const ARARAT_BACKGROUND = require('../../assets/backgrounds/ararat4.jpeg');
+const ARARAT_BACKGROUND = require('../../assets/backgrounds/game_backgrounds/street_armo.png');
 
 type Props = {
   /** Opacity of a dark overlay on top of the image (0-1, default 0.3) */
