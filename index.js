6bab7b1b702a19f9df1dfb76dcc3a82251aa4ecc
ajp-sourceship <ajp2914@gmail.com>
2026-05-09 15:35:21 -0500

    feat: suppress debugger connection warnings during development

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ import '@react-native-firebase/app';
 // Must be imported before uuid or any crypto-dependent libraries
 import 'react-native-get-random-values';
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+// Hide the yellow-box "not connected to debugger" / remote-debugger warnings
+// while developing on-device. Errors and other warnings still surface normally.
+LogBox.ignoreLogs([
+  /debugger/i,
+  /Remote debugger/i,
+  /Connection to .* debugger/i,
+  /No apps connected/i,
+  /not connected to a debugger/i,
+]);
 
 AppRegistry.registerComponent(appName, () => App);
