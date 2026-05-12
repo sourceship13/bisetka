6785b209a2d6bdb8eb3982ae276c47a09e59d278
ajp-sourceship <ajp2914@gmail.com>
2026-05-12 16:21:00 -0500

    feat: add starter shoe handling and integrate into AvatarBuilderScreen and OnboardingScreen

--- src/screens/Meta/Onboarding/OnboardingScreen.tsx
+++ src/screens/Meta/Onboarding/OnboardingScreen.tsx
@@ -28,6 +28,7 @@ import {
   getStarterShirtIdForAvatar,
   getStarterPantsIdForAvatar,
   getStarterHairIdForAvatar,
+  getStarterShoeIdForAvatar,
 } from '../../../data/clothingItems';
 import type { BaseAvatar } from '../../../types/avatar2d';
 
@@ -236,13 +237,16 @@ const OnboardingScreen: React.FC<{navigation: any; route?: any}> = ({navigation}
           const shirtId = getStarterShirtIdForAvatar(selectedGender, build);
           const pantsId = getStarterPantsIdForAvatar(selectedGender, build);
           const hairId = getStarterHairIdForAvatar(selectedGender);
+          const shoeId = getStarterShoeIdForAvatar(selectedGender);
           const shirt = ALL_CLOTHING_ITEMS.find(i => i.id === shirtId);
           const pants = ALL_CLOTHING_ITEMS.find(i => i.id === pantsId);
           const hair = ALL_CLOTHING_ITEMS.find(i => i.id === hairId);
+          const shoes = ALL_CLOTHING_ITEMS.find(i => i.id === shoeId);
           const equipped: Record<string, any> = {};
           if (shirt) equipped[shirt.type] = shirt;
           if (pants) equipped[pants.type] = pants;
           if (hair) equipped[hair.type] = hair;
+          if (shoes) equipped[shoes.type] = shoes;
           await AsyncStorage.setItem(
             '@bisetka_equipped_clothing',
             JSON.stringify(equipped),
@@ -255,6 +259,7 @@ const OnboardingScreen: React.FC<{navigation: any; route?: any}> = ({navigation}
           if (shirt) owned.add(shirt.id);
           if (pants) owned.add(pants.id);
           if (hair) owned.add(hair.id);
+          if (shoes) owned.add(shoes.id);
           await AsyncStorage.setItem(
             'ownedClothing',
             JSON.stringify([...owned]),
